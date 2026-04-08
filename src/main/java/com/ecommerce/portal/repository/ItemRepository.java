b45c58a9b8649e6a0140cd24f05ba143e2800a42
prakashsingh03 <prakash.singh03@nagarro.com>
2026-04-08 18:31:27 +0530

    fixed build

--- src/main/java/com/ecommerce/portal/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.portal.repository;
-
-import com.ecommerce.portal.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.awt.*;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
--- src/main/java/com/ecommerce/portal/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.portal.repository;
-
-import com.ecommerce.portal.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
