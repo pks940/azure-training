d8826360497c8379b2c0ea22d13c2862d6391b5d
prakashsingh03 <prakash.singh03@nagarro.com>
2026-04-10 14:11:56 +0530

    fixed build

---
+++ src/main/java/com/ecommerce/portal/entity/Item.java
@@ -0,0 +1,24 @@
+package com.ecommerce.portal.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Item {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private double price;
+    private int quantity;
+
+}
